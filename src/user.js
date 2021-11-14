91de0b807b535eb9f29ae4f09f23ca1aa966f805
QiangLoozen <74371458+QiangLoozen@users.noreply.github.com>
2021-11-14 20:14:13 +0100

    finished section 10

--- src/user.js
+++ src/user.js
@@ -19,8 +19,16 @@ const UserSchema = new Schema({ // Regels waaraan het model moet voldoen
     }]
 });
 
+//Virtual property
 UserSchema.virtual('postCount').get(function() {
     return this.posts.length; // this keyword werkt niet met fat arrowfuncties, dan wordt de context van dit bestand aangeroepen
+});
+
+//Middleware
+UserSchema.pre('remove', function(next) {
+    const BlogPost = mongoose.model('blogPost');
+    BlogPost.remove({_id: {$in: this.blogPosts}}) // if _id is in joe's blogPosts id-array
+        .then(() => next());
 });
 
 const User = mongoose.model('user', UserSchema); // Model Object, vergelijkbaar met een DBset van C#
